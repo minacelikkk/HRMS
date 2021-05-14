eebf8c5a483cd1f6ac2c10b71d1532a8d2a6ddaf
MinaVeyisoglu <MinaVeyisoglu@DESKTOP-8696LFL>
2021-05-14 17:41:29 +0300

    hrms

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,24 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Table(name="job_positions")
+@Entity
+public class JobPosition {
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="position_name")
+	private String position_name;
+
+
+}
