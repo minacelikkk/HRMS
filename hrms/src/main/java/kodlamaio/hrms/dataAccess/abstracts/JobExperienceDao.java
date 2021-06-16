6188a0601367ad8e67c5569aff7f084675e6b4a2
MinaVeyisoglu <MinaVeyisoglu@DESKTOP-8696LFL>
2021-06-16 15:15:48 +0300

    code refactored

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobExperienceDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobExperienceDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import kodlamaio.hrms.entities.concretes.JobExperience;
